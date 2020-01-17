4fa2a6cd19379fd4386a0ab6c26f4ba1e5039276
Eugenio Nappi <33025943+napp1@users.noreply.github.com>
2020-01-17 15:36:38 +0100

    Update rgb2img.py

--- rgb2img.py
+++ rgb2img.py
@@ -5,7 +5,7 @@ import openpyxl
 file_xlsx = openpyxl.load_workbook('file.xlsx')
 sheet = file_xlsx['Foglio1']
 
-#legge file xlsx e per ogni riga colore crea l'immagine
+#legge file xlsx e per ogni riga colore e crea l'immagine
 #il range dipende dal numero di righe del foglio di calcolo
 for row_position in range(2, 203):
     codice=sheet.cell(row=row_position, column=1).value
@@ -13,6 +13,5 @@ for row_position in range(2, 203):
     color_g=int(sheet.cell(row=row_position, column=3).value)
     color_b=int(sheet.cell(row=row_position, column=4).value)
     print(codice, color_r, color_g, color_b)
-    #dimensione in pixel dell'immagine (es.1000x1000)
-	img = Image.new('RGB', (1000, 1000), color = (color_r, color_g, color_b))
+    img = Image.new('RGB', (1000, 1000), color = (color_r, color_g, color_b))
     img.save(codice+'.png')
